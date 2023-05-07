a09db9dd2562d85209239d07ff484c4f173ae682
i306534 <vadim.rommer@sap.com>
2023-05-07 10:23:13 +0300

    Add tests

--- src/test/groovy/com/example/demo/DemoApplicationTests.groovy
+++ src/test/groovy/com/example/demo/DemoApplicationTests.groovy
@@ -10,4 +10,53 @@ class DemoApplicationTests {
 	void contextLoads() {
 	}
 
+	// Dies from under population
+	@Test
+	void diesWithOneNeighbor() {  // Any live cell with fewer than two live neighbours dies, as if by underpopulation.
+
+	}
+
+	@Test
+	void diesWithZeroNeighbors() {  // Any live cell with fewer than two live neighbours dies, as if by underpopulation.
+
+	}
+
+	// Continues to next generation
+	@Test
+	void continuesWithTwoNeighbors() { // Any live cell with two or three live neighbours lives on to the next generation.
+
+	}
+
+	@Test
+	void continuesWithThreeNeighbors() {
+
+	}
+
+	// Over Population
+	@Test
+	void diesWithFourLivingNeighbors() { // Any live cell with more than three live neighbours dies, as if by overpopulation
+
+	}
+
+	@Test
+	void deadCellGenerates() { // Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction
+
+	}
+
+	@Test
+	void deadCellWithSingleNeighbor() {	// Dead cell doesn't generate
+
+	}
+
+	@Test
+	void deadCellWithNoNeighbors() {	// Dead cell doesn't generate
+
+	}
+
+	@Test
+	void deadCellWithAllNeighbors() {	// Dead cell doesn't generate
+
+	}
+
+
 }
